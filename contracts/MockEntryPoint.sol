2aac6fbfb6d3c6b64af91a7eac1b1aa77887f62c
Alexandre <alexandrewlff@gmail.com>
2025-04-13 18:50:14 +0200

    Remove mock entrypoint

--- contracts/MockEntryPoint.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-import {EntryPoint} from "@account-abstraction/contracts/core/EntryPoint.sol";
-
-contract MockEntryPoint is EntryPoint {}
